bdce5abafcfc5e6764598051d8604fdfb9c4d58a
hood1235 <2544563843@qq.com>
2023-01-20 19:01:50 +0800

    2023-1-20-3

--- myApp/src/components/Developer/GameUpLoad.vue
+++ myApp/src/components/Developer/GameUpLoad.vue
@@ -2,10 +2,11 @@
   <div class="main">
 
     <div class="form">
-      发布游戏
+      <a style="position: relative;left: 42%">发布游戏</a>
       <n-form
         ref="formRef"
         :model="formValue"
+        :rules="rules"
         style="margin-left: 5%"
       >
         <n-form-item label="游戏名称" path="gameName">
@@ -28,26 +29,19 @@
           <n-input v-model:value="formValue.about" round type="textarea" placeholder="关于这款游戏（内容、玩法）" style="width: 80%" />
         </n-form-item>
       </n-form>
-      <n-button type="primary" @click="upload">
+      <n-button type="primary" style="margin-left:42%;width: 150px;height: 50px" @click="upload">
         发布
       </n-button>
     </div>
 
       <n-upload
           action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
-          :default-file-list="previewFileList"
+          v-model:file-list="fileList"
           list-type="image-card"
-          @preview="handlePreview"
-          style="position: relative;left: 48vw;top: 14vh"
+          @change="handleUploadChange"
+          @update:file-list="handleFileListChange"
+          style="position: relative;left: 48vw;top: 13vh;height: 220px"
       />
-      <n-modal
-          v-model:show="showModal"
-          preset="card"
-          style="width: 600px"
-          title="一张很酷的图片"
-      >
-        <img :src="previewImageUrl" style="width: 100%">
-      </n-modal>
 
   </div>
 </template>
@@ -55,7 +49,9 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import axios from "axios";
+import { useMessage } from 'naive-ui'
 
+const message = useMessage()
 const formRef=ref()
 const formValue=ref({
   name:'',
@@ -65,6 +61,34 @@ const formValue=ref({
   introduce:'',
   about:'',
 })
+const rules={
+  gameName:[{
+    required:true,
+    message:'请输入游戏名称',
+    trigger:["input","blur"]
+  }],
+  price:[{
+    required:true,
+    message:'请输入价格',
+    trigger:["input","blur"]
+  }],
+  introduce:[{}],
+  about:[{}],
+}
+
+const fileList=ref([])
+
+const handleUploadChange=(data: { fileList })=> {
+  fileList.value = data.fileList
+}
+
+const handleFileListChange = () => {
+  if (fileList.value.length>4){
+    fileList.value.pop()
+    message.error('最多上传4张')
+  }
+}
+
 const upload = () => {
   formValue.value.name='hood1235'
   axios.post("/game/upload",formValue.value).then((respon)=>{
@@ -87,7 +111,7 @@ const upload = () => {
   margin-top: 5%;
   background-color: #181A21;
   border-radius:2em;
-  text-align: center;
+  text-align: left;
   color: #b8b6b4;
   font-size: 36px;
 }
